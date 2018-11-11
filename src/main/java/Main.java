014f933d863f59c55188e23e38732f1f91f5cd49
huseyin-altunbas <huseyin.altunbas@tutkal.com.tr>
2018-11-11 17:33:34 +0300

    Inner Join Sample

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import com.datastax.spark.connector.japi.CassandraRow;
 import com.datastax.spark.connector.japi.rdd.CassandraTableScanJavaRDD;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.cassandra.CassandraSQLContext;
 import org.apache.zookeeper.data.Stat;
 
 import java.util.*;
@@ -45,5 +49,57 @@ public class Main {
 
 
 
+    }
+
+
+    public static  void InnerJoin()
+    {
+
+        SparkConf conf = new SparkConf(true)
+                .set("spark.cassandra.connection.host", "192.168.1.85");
+        //.set("spark.cassandra.auth.username", "cloud")
+        //.set("spark.cassandra.auth.password", "scape");
+        conf.setAppName("Java API demo");
+        conf.setMaster("local");
+        //conf.set("spark.driver.allowMultipleContexts", "true");
+
+
+        //JavaSparkContext sc = new JavaSparkContext(conf);
+        // SQLContext sqlContext = new SQLContext(sc);
+
+
+
+        SparkContext sparkContext = new SparkContext(conf);
+        CassandraSQLContext csc = new CassandraSQLContext(sparkContext);
+        csc.setKeyspace("analytics");
+        org.apache.spark.sql.DataFrame dataFrame = csc.sql("SELECT page.url as page_url, cursor.url as cursor_url FROM page INNER JOIN cursor on cursor.url=page.url");
+        long ff =  dataFrame.count();
+    }
+    public static  void SaveDataFrame()
+    {
+
+        SparkConf conf = new SparkConf(true);
+        conf.setAppName("Java API demo");
+        conf.setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        SQLContext sqlContext = new SQLContext(sc);
+
+
+
+
+        DataFrame dataframe =  sqlContext.read()
+                .format("org.apache.spark.sql.cassandra")
+                .load();
+
+                /*
+        DataFrame dataframe = sqlContext.read()
+                .format("org.apache.spark.sql.cassandra")
+                .options(ImmutableMap.of("table", "kv", "analytics", "page"))
+                .load();
+
+        dataframe.saveAsTable("page");
+*/
     }
 }
